37f58688cae3c134fb937f60106b1629fcdee34b
Dan Brioli <technicaldebtgames@gmail.com>
2020-09-20 12:40:32 -0400

    Initial commit and first essay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>Video Essay Transcripts</h1>
+
       </header>
+    
+      <article>
+
+        <h2>1. Aesthetics</h2>
+
+        <p>Aesthetics is generally defined as the study of beauty. It's also often defined in its relation to philosophy, a branch of study on its own.</p>
+
+        <p>In my opinion, it deals with the metaphysical. You can't touch the concept of beauty or style. You can't quantify some AMOUNT of it. You can't put beauty, the concept, in a bucket. It's as intimate and ephemeral a concept as time, or space, or hate, or love, or envy.</p>
+
+        <p>Given the difficult-to-pin-down nature of aesthetics, what do we mean when we talk about AN aesthetic? Used as a noun in this case, AN aesthetic usually means the principles used to guide or generate a certain type of art, or a design, or a style, or the mood of a work.</p>
+
+        <p>What is the aesthetic of, say... this video you are watching, right now? How would we explain WHAT its aesthetic is?</p>
+
+        <p>We might try to qualify or quantify it in some way - we might try to express, through language, the qualities that AN aesthetic is comprised of.</p>
+
+        <p>Grainy-looking digital video captured from a cheap webcam in a poorly-lit room. A glow from a nearby screen, or two. Audio is clear because the microphone is right next to his face, however, creating a strange dichotomy, although... Dichotomy might not be the right word... The person in front of the camera is blindfolded - is it a nod to justice? To oracles? A firing squad? He's reciting a monologue. The pacing of his language is measured. We might get an impression of thoughtfulness. It's obviously scripted, right? We might feel that he is being overly dramatic. We might be bored.</p>
+
+        <p>Wait... Somewhere along the way in qualifying this video's aesthetic, we took some kind of a detour. We began by describing the qualities of the production - its poor video quality, its higher audio quality, etc. While these are relative descriptors, they are still grounded in real things we could possibly measure. We could find out what camera is being used and compare it to others and their various specifications. We could find out what lights are around the speaker, and see how many lumens they produce, and in what frequencies.</p>
+
+        <p>Admittedly, right there, we spoke of qualities - poor video quality, higher audio quality, and so on. But then, in trying to capture our meaning of words like "poor, better, worse, higher," we broke down and started QUANTIFYING these qualities. We talked about brands and models of cameras and microphones. We talked about light levels, with lumens and frequencies. These quantities, related to each other in a relative way, made us FEEL like the production's quality was good, or bad.</p>
+
+        <p>However, in our original examination of the scene, given a bit of time to see past the obvious visual and audio qualities of the production, we eventually started to describe how we FELT about it, but through concepts. We explained how we were reminded of these concepts when we viewed the work. Some, like justice, are as difficult to pin down as aesthetics, and love, and hate, and envy. Others, like firing squads, are grounded in concrete reality. We know that there is a concept known as firing squads and actual examples of this concept exist in our reality. There are also stories and historical records of oracles, or at least people claiming to be them. Concepts such as justice, however...</p>
+
+        <p>But once again, we are losing the plot on trying to find WHAT an aesthetic is, aren't we? We can't seem to pin it down on any one particular thing. Going back to our original view of the work, we ended up talking about how we FELT, how the work made us FEEL. We felt like the work was overly dramatic, or we felt like the work was thoughtful, or we felt that it was boring. We might consider that these words have positive or negative connotations with them that we associate seemingly automatically. Boring is negative. Overly dramatic is negative. Thoughtful is positive.</p>
+
+        <p>And yet if we dig into this further, we realize that positive and negative aren't really grounded in reality. There is no reason, no explicit and inherent reason at least, as to why boring needs to have a negative connotation. Most of everyone's life is boring, if you really think about it. The vast majority of our time is spent wading through boredom, trying to get to the "good parts". Given the quantity of this boredom in our lives, between the commutes, and the dead air, and waiting for the next thing to happen at some random time in the future that isn't the here and now... Doesn't it feel like a shame that it is seen as such a negative? And why is "thoughtful" a positive, anyway? The idiom "ignorance is bliss" exists for a reason, doesn't it?</p>
+
+        <p>It seems like the concepts of "positive" or "negative" aren't able to really be captured easily, either. It seems like they're as ephemeral as AN aesthetic. Perhaps they were just drilled into us over the years, patterns we were taught or learned in some way. Is the protestant work ethic worth defending for some objective reason? Should we really be bored if we have a moment to ourselves in which we can think, alone, with no one else around, and with nothing to distract us from just experiencing... us?</p>
+
+        <p>*sigh*</p>
+
+        <p>We keep getting distracted in our hunt for the aesthetic. We keep trying to understand exactly where this "aesthetic" is located, but we keep going down tangential rabbit holes. It slips through our grasp of language. It's clearly here, like gravity, pulling us in some direction, but the direction seems to be different for each person. We can quantify certain qualities, but they don't seem to be the true thing we are trying to describe. Everything seems to just be words covering up unquantifiable qualities that we FEEL.</p>
+
+        <p>If I say, "Imagine a tree," what do you imagine in your mind's eye? Take a few seconds to imagine your tree. I mean REALLY imagine it. Picture the color and size and the limbs and the leaves and the trunk and the profile...</p>
+
+        <p>*pause*</p>
+
+        <p>And now, when I say, "Imagine a large tree," what do you imagine? Has it changed? If so, how? If not, why?</p>
+
+        <p>And then if I say, "Imagine a large green tree..." Well, it was probably green in your mind already, but it COULD have been fall in your imaginary tree's environment. Why was it not fall? Is it because where you live doesn't have that type of season? Is it because it's summer right now? Or maybe you DID imagine a tree in fall, with red and yellow leaves, half of which are on the ground below.</p>
+
+        <p>And if I say, "Imagine a large green PINE tree," what do you imagine? Wait! It was a deciduous tree before! The tree has changed completely! Or, maybe you live in rural Alaska, and every tree around you is a pencil-thin pine, the branches snapped off from sub-zero months of winter every year.</p>
+
+        <p>I am starting to quantify the concept and style - the AESTHETIC - of a tree, transmitted from one person to another, through more and more language. Depending on where you are in the world, you may never even have SEEN a pine tree, or you may only have ever seen one on a screen, Plato's cave made manifest.</p>
+
+        <p>I mean... depending on where you are in the world or in time, a tree could mean any number of things. A person who grew up in a nuclear fallout bunker and has worked on post-apocalyptic robots their entire life under the harsh glow of LED lighting might imagine a binary search tree, perhaps, not even knowing where it got its name from...</p>
+
+        <p>Regardless, I can continue to describe this tree, using more and more specific qualifiers, and at some point your mind WILL be overwhelmed. You won't be able to keep all of these qualifiers and quantifiers in your conscious mind. You WILL lose details. If I were to say there were X number of branches, you will eventually forget and imagine the tree with Y number of branches. The SALIENCE of details changes as a function of time and the sheer number of other details your consciousness needs to keep track of.</p>
+
+        <p>We might think about this and come to the conclusion that, perhaps, AN aesthetic is that set of qualities that remain the most salient to us if we were to try to describe the scene to another person. The most immediate and pressing descriptors that we FEEL we must use to describe the production to them, knowing inherently that the human mind can't keep juggling details forever. We might try to take a bit from different and unique... "categories" of experience. What is the most striking VISUAL quality? What is the most striking AUDIO quality? What is the most striking EMOTIONAL quality? Where do these categories come from?</p>
+
+        <p>Grainy and dark. Clearly spoken. Monologue. Blindfold. ..Justice? ...Oracle? ....Thoughtful?</p>
+
+        <p>And while we all may have pine trees or deciduous trees in our mind, we do remember that it was a LARGE GREEN tree.</p>
+
+        <p>And we might agree that THAT is what AN aesthetic is - a few salient qualifiers in our conscious mind that describe the primary concepts the work reminds us of, while we fill in less important gaps automatically through the magic of the subconscious, all the while hoping that we were accurate enough to get the point across.</p>
+
+      </article>
+
+      <footer>
+
+        <h4>Copyright Dan Brioli 2020-</h4>
+
+      </footer>
+
     </div>
   );
 }
